74e04772bc01b114be5ad1fbe7b4b42af152d560
Genesis Maria Nobrega Betencourt <78150653+GenesisDN@users.noreply.github.com>
2024-04-02 17:30:35 +0100

    changes

--- Assets/Dog/DogAgentController.cs
+++ Assets/Dog/DogAgentController.cs
@@ -9,18 +9,17 @@ public class DogAgentController : MonoBehaviour
     private Vector3 targetPosition;
     private Rigidbody rb;
     private bool isMoving = false;
-    private bool isFetchig = false;
+    private bool isFetching = false;
     private bool isCarrying = false;
-    private string movementAnimation = "MoveRun";
-    private string targetTag;
+    private bool canFollow = true;
     private float moveSpeed = 0.5f;
     public float walkingTresholdDistance = 1f;
     public float walkSpeed = 0.2f;
     public float runSpeed = 0.5f;
-    public float swimSpeed = 0.1f;
     public SphereCollider characterCollider;
     public Camera cameraObject;
     public LayerMask terrainLayerMask;
+    public GameObject bone;
 
     void Start()
     {
@@ -31,23 +30,81 @@ public class DogAgentController : MonoBehaviour
 
     void FixedUpdate()
     {
-        FollowCharacter();
+        if (isFetching) FetchBone();
+        if (canFollow) FollowCharacter();
         if (isMoving)
         {
             MoveToTarget();
-            //TargetReach();
+            if (isFetching) PickUp();
+            
         }
     }
 
-    private void SetTargetTagPosition()
+    private void FollowCharacter()
     {
-        isFetchig = true;
-        GameObject targetObject = GameObject.FindWithTag(targetTag);
+        SetPlayerTargetPosition();
 
-        if (targetTag == "Bone")
+        if (dogCollider.bounds.Intersects(characterCollider.bounds) ||
+           Vector3.Distance(transform.position, cameraObject.transform.position) < 0.5f)
         {
-            Vector3 targetObjectPosition = GetLongerSideOfBone(targetObject);
-            
+            isMoving = false;
+            HandleMovementAnimation();
+            if (isCarrying) DropObject();
+            return;
+        }
+    }
+
+    private void SetPlayerTargetPosition()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(characterCollider.transform.position, Vector3.down);
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, terrainLayerMask))
+        {
+            Vector3 newPosition = hit.point;
+            newPosition.y += 0.03f;
+
+            Vector3 cameraPosition = cameraObject.transform.position;
+            Vector3 cameraForward = cameraObject.transform.forward;
+            Vector3 position = cameraPosition + (cameraForward * 0.5f);
+            newPosition.x = position.x;
+            newPosition.z = position.z;
+
+            this.targetPosition.y = newPosition.y;
+            this.targetPosition.x = position.x;
+            this.targetPosition.z = position.z;
+
+            this.isMoving = true;
+            HandleMovementAnimation();
+        }
+    }
+
+    public void StartFetching()
+    {
+        isFetching = true;
+        canFollow = false;
+    }
+
+    public void FetchBone()
+    {
+        if (bone == null) return;
+        if (isCarrying) return;
+        
+        Vector3 targetObjectPosition = GetLongerSideOfBone(bone);
+        RaycastHit hit;
+        Ray ray = new Ray(targetObjectPosition, Vector3.down);
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, terrainLayerMask))
+        {
+            Vector3 newPosition = hit.point;
+            newPosition.y += 0.01f;
+
+            this.targetPosition.y = newPosition.y;
+            this.targetPosition.x = newPosition.x;
+            this.targetPosition.z = newPosition.z;
+
+            this.isMoving = true;
+            HandleMovementAnimation();
         }
     }
 
@@ -59,69 +116,9 @@ public class DogAgentController : MonoBehaviour
         {
             longerSideDirection = bone.transform.right;
         }
-        return bone.transform.position + (longerSideDirection.normalized * 0.2f);
+        return bone.transform.position + (longerSideDirection.normalized * 0.01f);
     }
 
-    private void OnReachRotateToTarget()
-    {
-        GameObject boneObject = GameObject.FindWithTag(targetTag);
-        BoxCollider boneCollider = boneObject.GetComponent<BoxCollider>();
-
-        if (boneCollider != null)
-        {
-            Vector3 boneSize = boneCollider.size;
-            Vector3 longerSideDirection;
-            if (boneSize.x > boneSize.z)
-            {
-                longerSideDirection = boneObject.transform.right;
-            }
-            else
-            {
-                longerSideDirection = boneObject.transform.forward;
-            }
-            Vector3 centerPointOfLongerSide = boneObject.transform.position + longerSideDirection * boneSize.magnitude / 2f;
-            Quaternion targetRotation = Quaternion.LookRotation(centerPointOfLongerSide - transform.position);
-            rb.MoveRotation(targetRotation);
-        }
-        else
-        {
-            Debug.LogWarning("Bone object does not have a BoxCollider.");
-        }
-    }
-
-    private void FollowCharacter()
-    {
-        if (dogCollider.bounds.Intersects(characterCollider.bounds) ||
-            characterCollider.bounds.Contains(dogCollider.ClosestPoint(dogCollider.bounds.center)))
-        {
-            this.isMoving = false;
-            HandleMovementAnimation();
-            return;
-        }
-
-        RaycastHit hit;
-        Ray ray = new Ray(characterCollider.transform.position, Vector3.down);
-
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, terrainLayerMask))
-        {
-            Vector3 newPosition = hit.point;
-            newPosition.y += 0.01f;
-
-            Vector3 cameraPosition = cameraObject.transform.position;
-            Vector3 cameraForward = cameraObject.transform.forward;
-            Vector3 position = cameraPosition + (cameraForward * 2f);
-            newPosition.x = position.x;
-            newPosition.z = position.z;
-
-            this.targetPosition.y = newPosition.y;
-            this.targetPosition.x = position.x;
-            this.targetPosition.z = position.z;
-
-            this.isMoving = true;
-            HandleMovementAnimation();
-        }
-        
-    }
 
     private void MoveToTarget()
     {
@@ -130,25 +127,38 @@ public class DogAgentController : MonoBehaviour
         rb.MoveRotation(Quaternion.LookRotation(rb.velocity));
     }
 
-    private void TargetReach()
+
+    private void PickUp()
     {
-        if (dogCollider.bounds.Intersects(characterCollider.bounds) ||
-            characterCollider.bounds.Contains(dogCollider.ClosestPoint(dogCollider.bounds.center)))
-        {
+        if (Vector3.Distance(rb.transform.position, bone.transform.position) < 0.5f) {
+            isFetching = false;
+            isMoving = false;
+            isCarrying = true;
+
+            HandleMovementAnimation();
             
-            if (isFetchig)
-            {
-                OnReachRotateToTarget();
-                PickUp();
-                isFetchig = false;
-            }
+            animator.SetTrigger("PickUp");
+            animator.SetBool("Carry", true);
         }
+    }
+
+    public void DropObject()
+    {
+        Vector3 directionToCamera = Camera.main.transform.position - transform.position;
+        directionToCamera.y = 0f;
+        transform.rotation = Quaternion.LookRotation(directionToCamera);
+
+        isCarrying = false;
+        animator.SetBool("Carry", false);
+        animator.SetTrigger("PutDown");
     }
 
     private void MountBone()
     {
         GameObject mouth = GameObject.FindWithTag("Mouth");
-        GameObject bone = GameObject.FindWithTag("Bone");
+        Rigidbody boneRb = bone.GetComponent<Rigidbody>();
+        boneRb.isKinematic = true;
+
         bone.transform.parent = mouth.transform;
         bone.transform.localPosition = Vector3.zero;
         bone.transform.localRotation = Quaternion.identity;
@@ -156,35 +166,25 @@ public class DogAgentController : MonoBehaviour
 
     private void UnmountBone()
     {
-        GameObject bone = GameObject.FindWithTag("Bone");
         GameObject dogHead = GameObject.FindWithTag("DogHead");
 
         if (bone != null && dogHead != null)
         {
-            // Calculate the drop position directly below the dog's head
-            Vector3 dropPosition = dogHead.transform.position;
-            dropPosition.y = 0.05f;
-
-            RaycastHit hit;
-            if (Physics.Raycast(dropPosition, Vector3.down, out hit, Mathf.Infinity, LayerMask.GetMask("Terrain")))
-            {
-                // Adjust the drop position to be on the ground level
-                dropPosition = hit.point;
-            }
-
             bone.transform.SetParent(null);
-            bone.transform.position = dropPosition;
-            bone.transform.rotation = Quaternion.identity;
+            Rigidbody boneRb = bone.GetComponent<Rigidbody>();
+            boneRb.isKinematic = false;
         }
     }
 
-    private void PickUp()
+    public void OnPickUp()
     {
-        if (targetTag != "Bone") return;
-        isCarrying = true;
-        targetTag = null;
-        animator.SetTrigger("PickUp");
-        animator.SetBool("Carry", true);
+        MountBone();
+        canFollow = true;
+    }
+
+    public void OnPutDown()
+    {
+        UnmountBone();
     }
 
     private void HandleMovementAnimation()
@@ -213,47 +213,11 @@ public class DogAgentController : MonoBehaviour
 
     }
 
-    public void SetTargetTag(string targetTag)
-    {
-        GameObject targetObject = GameObject.FindWithTag(targetTag);
-        if (targetObject == null) return;
-        if (isCarrying) return;
-        this.targetTag = targetTag;
-        HandleMovementAnimation();
-        SetTargetTagPosition();
-    }
-
     public void Pet()
     {
-        if (!isMoving && !isFetchig && !isCarrying)
+        if (!isMoving && !isFetching && !isCarrying)
         {
             animator.SetTrigger("PetMiddle");
         }
-    }
-
-    public void OnPickUp()
-    {
-        MountBone();
-    }
-
-    public void DropObject()
-    {
-        if (isCarrying)
-        {
-            isCarrying = false;
-            animator.SetBool("Carry", false);
-            animator.SetTrigger("PutDown");
-        }
-    }
-
-    public void OnPutDown()
-    {
-        UnmountBone();
-    }
-
-    public void StopMovement()
-    {
-        isMoving = false;
-        animator.SetBool(movementAnimation, false);
     }
 }
